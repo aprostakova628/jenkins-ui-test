b8205321c09011bda5da6f4134087f0ebfd0f3f3
prostakova <aprostakova628@gmail.com>
2018-10-08 22:46:45 +0300

    add negative scenario for login test

--- test/org/selenide/uitests/JenkinsTest.java
+++ test/org/selenide/uitests/JenkinsTest.java
@@ -2,9 +2,7 @@ package org.selenide.uitests;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.value;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -32,8 +30,22 @@ public class JenkinsTest {
     $(By.name("Submit")).click();
     $(By.id("jenkins-home-link")).shouldBe(visible);
 
+  }
 
+  @Test
+  public void JenkinsLogInNegative()
+  {
+    open("http://ec2-18-184-229-190.eu-central-1.compute.amazonaws.com:8080");
+    $(By.name("j_username")).val("admin");
+    $(By.name("j_password")).val("12345");
+    $(By.name("Submit")).click();
+    $(By.className("alert alert-danger"));
+    $(By.name("login")).shouldHave(text("Invalid username or password"));
+    $(By.className("danger")).should(exist);
 
+    //TODO add check for danger class login\password separately
 
   }
+
+  //TODO add check for "Keep me signed in" (without cookie)
 }
