b7d0733774521a7185af254008fb2a4fd3bb5ea8
k.sergievskiy <sergievskiy@outlook.com>
2018-11-19 13:40:07 +0300

    Basic Checks in @BeforeClass

--- test/org/selenide/uitests/JenkinsTest.java
+++ test/org/selenide/uitests/JenkinsTest.java
@@ -4,25 +4,31 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
 
 
+
 public class JenkinsTest {
-  String baseurl = "http://localhost:8080";
-  String username = "k.sergievskiy";
-  String email = "sergievskiy@outlook.com";
+  public static String baseurl = "http://localhost:8080";
+  public static String username = "k.sergievskiy";
+  public static String email = "sergievskiy@outlook.com";
+  public static String login = "admin";
+  public static String password = "Defaultpassw0rd";
+
+
     public void LogInPositive(){
         open(baseurl);
-        $(By.name("j_username")).val("admin");
-        $(By.name("j_password")).val("Defaultpassw0rd");
+        $(By.name("j_username")).val(login);
+        $(By.name("j_password")).val(password);
         $(By.name("Submit")).click();
     }
     public void LogInNegative(){
         open(baseurl);
-        $(By.name("j_username")).val("admin");
+        $(By.name("j_username")).val(login);
         $(By.name("j_password")).val("2123");
         $(By.name("Submit")).click();
     }
     public void LogOutPositive(){
         open(baseurl + "/logout");
     }
+
 
 
 
